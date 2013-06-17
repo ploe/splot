a0649e7e2de929bb822c1fca8d3bebb40741e96a
Myke Atkinson <ploe@hotmail.co.uk>
2013-06-17 07:25:19 +0100

    Polished Splot interface
    	* added Markdown rendering
    	* tightened interface
    	* fixed weird bugs, that I can't remember now...

--- Splot.rb
+++ Splot.rb
@@ -1,35 +1,46 @@
 #! /usr/bin/ruby
 
 require 'dalli'
+require 'github/markdown'
 
 #	The splot interface is the class that sits between, me, memcached and the content. 
 
 class Splot
-	attr_accessor :dc, :article
+	attr_accessor :dc, :article, :user
 	
 	def initialize
 		options = {:namespace => "splot", :compress => true, :expires_in => 7200}
 		@dc = Dalli::Client.new('localhost:11211', options)
 	
 		@article = []
+		@user = {}
 		fetch
 
-		user = {}
 	end
 
-	# get the next article - might amend this so you can ask for a specific user
-	def next
-		a = @article.pop
+	#	get pops the next article and renders it - filter the user with the param
+	def next(u=nil)
+		if u and not user?(u) then return nil end
+	
+		a = nil	
+		while a = @article.pop do
+			if u and u != a[:user] then next
+			else break end
+		end
 		if not a then return nil end
-		body = @dc.get("#{a[:filename]}")
-		# chunk of logic here if we haven't pulled anything out of the cache or the timestamps don't match
-		if (not body or body[:modified] != a[:modified]) and File::file?(a[:filename]) and File::readable?(a[:filename])
+	
+		body = @dc.get(a[:filename])
+		if (not body or body[:modified] != a[:modified]) and readable?(a[:filename])
 			f = File.new(a[:filename], "r")
-			body = { :text => f.read, :modified => File::mtime(a[:filename]).to_i }
+			body = { :text => f.read, :modified => File::mtime(a[:filename]).to_i}
 			@dc.set("#{a[:filename]}", body)
 			f.close
 		end
-		body	
+
+		body[:text] = GitHub::Markdown.render_gfm(body[:text])
+		body[:title] = a[:filename].match(/\/home\/.*\/(.*)/).captures[0]	#	filthy...
+		body[:user] = a[:user]
+		body
 	end
 
 	#	populate pushes the article data in to the cache
@@ -41,12 +52,16 @@ class Splot
 			article = []
 			Dir.foreach(path) { |f|
 				p = "#{path}#{f}"
-				if File::file?(p) and File::readable?(p)
+				if readable?(p)
 					article.push({:filename => p, :modified => File::mtime(p).to_i })
 				end
 			}
 			@dc.set("#{u}-articles", article)
 		}
+	end
+
+	def user?(u)
+		@user[u]
 	end
 
 
@@ -59,18 +74,23 @@ private
 			tmp.each { |a|
 				a[:user] = u
 				@article.push(a)
-				# @user[u] = true
+				@user[u] = true
 			}
 		}
 		@article.sort! { |x, y|  x[:modified] <=> y[:modified]}
 	end
+
+	def readable?(f)
+		(File::file?(f) and File::readable?(f))
+	end
 	
 end
+# splot = Splot.new()
+# splot.populate
+# puts splot.user?('myke')
+# for i in 0..10
+#	puts splot.next
+#end
 
-splot = Splot.new()
-splot.populate
-for i in 0..10
-	puts splot.next
-end
 #m if splot.user["myke"] then puts "Yey, you exist." end
-puts splot.next
+#puts splot.next
